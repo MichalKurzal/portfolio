a5e6b4830a4dc63ecd53240acb938bdbbce2dd22
Michal Kurzal <michalkurzal200@gmail.com>
2020-07-13 17:24:47 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as Scrollmagic from 'scrollmagic'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'angular-portfolio';
+  maxwidth = window.innerWidth /3;
+  myInnerHeight = window.innerHeight;
+  myInnerWidth = window.innerWidth;
+  ngOnInit(){
+  window.addEventListener('resize',this.resize);
+  var controller = new Scrollmagic.Controller();
+  var scene = new Scrollmagic.Scene({
+    triggerElement: '.main'
+  })
+  .setClassToggle('.main' ,'show')
+  .addTo(controller)
+
+  var scene2 = new Scrollmagic.Scene({
+    triggerElement: '.div1'
+  })
+  .setClassToggle('.div1' ,'show')
+  .addTo(controller)
+  }
+  resize(){
+console.log('resize');
+let h = window.innerHeight;
+let w = window.innerWidth;
+document.getElementById('img1').style.height = h+'px';
+document.getElementById('img1').style.width = w+'px';
+document.getElementById('div1').style.height = h+'px';
+document.getElementById('div1').style.width = w+'px';
+document.getElementById('div2').style.height = h+'px';
+document.getElementById('div2').style.width = w+'px';
+document.getElementById('div3').style.height = h+'px';
+document.getElementById('div3').style.width = w+'px';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    MatCardModule,
+    MatDividerModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
