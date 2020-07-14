cc13d77cbede3c18a957a47b92c3a364b3a2005f
Michal Kurzal <michalkurzal200@gmail.com>
2020-07-14 03:35:53 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,10 @@ import * as Scrollmagic from 'scrollmagic'
 export class AppComponent implements OnInit{
   
   title = 'angular-portfolio';
+  centerW = window.innerWidth /2 -10;
   maxwidth = window.innerWidth /3;
   myInnerHeight = window.innerHeight;
-  myInnerWidth = window.innerWidth;
+  myInnerWidth = window.innerWidth-20;
   ngOnInit(){
   window.addEventListener('resize',this.resize);
   var controller = new Scrollmagic.Controller();
@@ -40,6 +41,7 @@ export class AppComponent implements OnInit{
   }
   resize(){
 console.log('resize');
+
 let h = window.innerHeight;
 let w = window.innerWidth;
 document.getElementById('img1').style.height = h+'px';
@@ -50,5 +52,10 @@ document.getElementById('div2').style.height = h+'px';
 document.getElementById('div2').style.width = w+'px';
 document.getElementById('div3').style.height = h+'px';
 document.getElementById('div3').style.width = w+'px';
+document.getElementById('imgs').style.left = this.centerW+'px';
+document.getElementById('menucard1').style.width = this.maxwidth+'px';
+document.getElementById('menucard2').style.width = this.maxwidth+'px';
+document.getElementById('menucard3').style.width = this.maxwidth+'px';
+this.ngOnInit();
   }
 }
